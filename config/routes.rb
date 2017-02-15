d433f61ebe43ce5b46b08c741126e717c7bfbd1f
Patrick Hoban & Pavan Sarguru <pair+patrickjameshoban.pksarguru@devbootcamp.com>
2017-02-15 17:28:22 -0600

    Completed TDD and implementation for user can create new game idea

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   mount Ckeditor::Engine => '/ckeditor'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'application#index'
-  resources :games, only: [:index, :show]
+  resources :games, only: [:index, :show, :create, :new]
 end
--- spec/controllers/games_controller_spec.rb
+++ spec/controllers/games_controller_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
 describe GamesController do
+  let!(:game) { Game.create!(name: "Battleship", description: "BUT WITH ROCKS", creator_id: 1)}
+
   describe 'GET #index' do
     it 'responds with status code 200' do
       get :index
@@ -15,6 +17,35 @@ describe GamesController do
     it 'renders the :index template' do
       get :index
       expect(response).to render_template(:index)
+    end
+  end
+
+  describe 'GET #new' do
+    it 'responds with status code 200' do
+      get :new
+      expect(response).to have_http_status 200
+    end
+
+    it 'renders the :new template' do
+      get :new
+      expect(response).to render_template(:new)
+    end
+  end
+
+  describe 'GET #show' do
+    it 'responds with status code 200' do
+      get :show, params: { id: game.id }
+      expect(response).to have_http_status 200
+    end
+
+    it "assigns the correct game as @game" do
+      get :show, params: { id: game.id }
+      expect(assigns(:game)).to eq(game)
+    end
+
+    it 'renders the :show template' do
+      get :show, params: { id: game.id }
+      expect(response).to render_template(:show)
     end
   end
 end
